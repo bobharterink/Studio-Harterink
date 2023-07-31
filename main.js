69a913fdf687fc4d4102209e30e6b574078d996f
bobharterink <49344016+bobharterink@users.noreply.github.com>
2023-07-31 16:34:43 +0200

    heel veel updates

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import GUI from "lil-gui";
 
-/* const gui = new GUI();
- */
+const gui = new GUI();
+
 let scene,
   renderer,
   mixer,
@@ -14,10 +14,18 @@ let scene,
   targetObject,
   targetObject1,
   targetObject2,
-  targetObject3;
+  targetObject3,
+  targetObject4,
+  targetObject5,
+  targetObject6,
+  targetObject7;
 let actions = [];
-let childObject = null; // Declare a variable to store the "hout24" child object
+let childObject = null; // Declare a variable to store the "bubbel24" child object
+let childObject1 = null; // Declare a variable to store the "golf24" child object
+let childObject2 = null; // Declare a variable to store the "knik24" child object
 let initialColor = 0x000000;
+let initialColor1 = 0x000000;
+let initialColor2 = 0x000000;
 let sphereInitialColor = 0xffffff;
 let sunLight, ambientLight;
 let sphereObject = null;
@@ -32,7 +40,11 @@ let sphere8Object = null;
 let sphere9Object = null;
 let sphere10Object = null;
 let cylinderObject = null;
+let cylinderObject1 = null;
+
 let cylinderInitialColor = 0x000000;
+let cylinderInitialColor1 = 0x000000;
+
 let sphere1InitialColor = 0x000000;
 var objectGroup = new THREE.Group();
 const clock = new THREE.Clock();
@@ -46,7 +58,7 @@ scene = new THREE.Scene();
 // Load GLB file using GLTFLoader
 const loader = new GLTFLoader();
 let sceneCamera = null;
-loader.load("/camera-path7.glb", (gltf) => {
+loader.load("/tstvaasanimation2.glb", (gltf) => {
   /*   objectGroup.add(gltf.scene);
    */ scene.add(gltf.scene);
   sceneCamera = gltf.scene.getObjectByName("test");
@@ -66,21 +78,21 @@ loader.load("/camera-path7.glb", (gltf) => {
   });
 
   // Add directional light with shadow casting
-  sunLight = new THREE.DirectionalLight("#ffffff", 0.5);
+  sunLight = new THREE.DirectionalLight("#ffffff", 0.4);
   sunLight.castShadow = true;
   sunLight.shadow.camera.far = 20;
   sunLight.shadow.mapSize.set(2048, 2048);
   sunLight.shadow.normalBias = 0.05;
-  sunLight.position.set(5.5, 7, 3);
+  sunLight.position.set(-2.5, 7, 3);
   scene.add(sunLight);
   /*   let sunLightHelper = new THREE.DirectionalLightHelper(sunLight, 5);
   scene.add(sunLightHelper);
   const cameraHelper = new THREE.CameraHelper(sunLight.shadow.camera);
   scene.add(cameraHelper); */
 
-  /*   gui.add(sunLight.position, "x", -10, 10, 0.01);
+  gui.add(sunLight.position, "x", -10, 10, 0.01);
   gui.add(sunLight.position, "y", -10, 10, 0.01);
-  gui.add(sunLight.position, "z", -10, 10, 0.01); */
+  gui.add(sunLight.position, "z", -10, 10, 0.01);
 
   function updateCamera() {
     sunLight.target.updateMatrixWorld();
@@ -114,10 +126,20 @@ loader.load("/camera-path7.glb", (gltf) => {
   cylinderObject = gltf.scene.getObjectByName("Cylinder");
   cylinderInitialColor = cylinderObject.material.color.clone();
 
-  // Set material color for child object with name "hout24"
-  // Set material color for child object with name "hout24"
-  childObject = gltf.scene.getObjectByName("hout24");
+  cylinderObject1 = gltf.scene.getObjectByName("standhoekig");
+  cylinderInitialColor1 = cylinderObject.material.color.clone();
+
+  // Set material color for child object with name "bubbel24"
+  childObject = gltf.scene.getObjectByName("bubbel24");
   initialColor = childObject.material.color.clone(); // Store the initial color
+
+  // Set material color for child object with name "golf24"
+  childObject1 = gltf.scene.getObjectByName("golf24");
+  initialColor1 = childObject1.material.color.clone(); // Store the initial color
+
+  // Set material color for child object with name "knik24"
+  childObject2 = gltf.scene.getObjectByName("knik24");
+  initialColor2 = childObject2.material.color.clone(); // Store the initial color
 
   // Make all objects receive shadow
   gltf.scene.traverse((child) => {
@@ -127,13 +149,50 @@ loader.load("/camera-path7.glb", (gltf) => {
     }
   });
 
-  targetObject = scene.getObjectByName("Asset_53_Asset_53001");
-  targetObject1 = scene.getObjectByName("ringbeneden");
-  targetObject2 = scene.getObjectByName("ringboven");
-  const group = new THREE.Group();
-
+  //vaas3 knik
+  const groupvaas3 = new THREE.Group();
   for (let i = 1; i <= 24; i++) {
-    const meshName = `hout${i}`;
+    const meshNamevaas3 = `knik${i}`;
+    const meshvaas3 = scene.getObjectByName(meshNamevaas3);
+
+    if (meshvaas3) {
+      groupvaas3.add(meshvaas3);
+    } else {
+      console.warn(`Mesh ${meshNamevaas3} not found in the GLB file.`);
+    }
+  }
+
+  targetObject5 = scene.getObjectByName("ringbeneden3");
+  targetObject6 = scene.getObjectByName("ringboven3");
+  targetObject7 = scene.getObjectByName("standhoekig");
+  groupvaas3.add(targetObject5);
+  groupvaas3.add(targetObject6);
+  groupvaas3.add(targetObject7);
+
+  scene.add(groupvaas3);
+
+  //vaas2 golf
+  const groupvaas2 = new THREE.Group();
+  for (let i = 1; i <= 24; i++) {
+    const meshNamevaas2 = `golf${i}`;
+    const meshvaas2 = scene.getObjectByName(meshNamevaas2);
+
+    if (meshvaas2) {
+      groupvaas2.add(meshvaas2);
+    } else {
+      console.warn(`Mesh ${meshNamevaas2} not found in the GLB file.`);
+    }
+  }
+  targetObject3 = scene.getObjectByName("ringbeneden2");
+  targetObject4 = scene.getObjectByName("ringboven2");
+  groupvaas2.add(targetObject3);
+  groupvaas2.add(targetObject4);
+  scene.add(groupvaas2);
+
+  //vaas 1 bubbel
+  const group = new THREE.Group();
+  for (let i = 1; i <= 24; i++) {
+    const meshName = `bubbel${i}`;
     const mesh = scene.getObjectByName(meshName);
 
     if (mesh) {
@@ -142,23 +201,20 @@ loader.load("/camera-path7.glb", (gltf) => {
       console.warn(`Mesh ${meshName} not found in the GLB file.`);
     }
   }
-
+  targetObject1 = scene.getObjectByName("ringbeneden");
+  targetObject2 = scene.getObjectByName("ringboven");
   group.add(targetObject1);
   group.add(targetObject2);
-  console.log(group.children);
-
   scene.add(group);
 
-  targetObject3 = scene.getObjectByName("Asset_157");
-
-  if (targetObject) {
+  if (groupvaas2) {
     // Hide the object initially
-    targetObject.visible = false;
+    groupvaas2.visible = false;
   }
 
-  if (targetObject3) {
+  if (groupvaas3) {
     // Hide the object initially
-    targetObject3.visible = false;
+    groupvaas3.visible = false;
   }
 
   imageElement1.addEventListener("click", toggleObjectVisibility1, true);
@@ -167,44 +223,75 @@ loader.load("/camera-path7.glb", (gltf) => {
 
   let lastClickedElement = imageElement1; // Variable to store the last clicked element
 
+  let clickedCylinderColor = new THREE.Color(0xb8abd4);
+
   function toggleObjectVisibility1() {
     if (lastClickedElement === imageElement1 && group.visible) {
       return; // Skip toggling if the same element is clicked and it's already visible
     }
 
     group.visible = !group.visible;
-    targetObject.visible = false;
-    targetObject3.visible = false;
+    cylinderObject.visible = true;
+    groupvaas2.visible = false;
+    groupvaas3.visible = false;
 
     lastClickedElement = imageElement1; // Update the last clicked element
+
+    //background color change
+    const bkgrncolor = new THREE.Color(0xfaf4eb);
+    renderer.setClearColor(bkgrncolor);
+
+    //stand change
+    const newColor = new THREE.Color(0xb8abd4);
+    cylinderObject.material.color = newColor;
+
+    clickedCylinderColor = newColor;
   }
 
   function toggleObjectVisibility2() {
-    if (lastClickedElement === imageElement2 && targetObject.visible) {
+    if (lastClickedElement === imageElement2 && groupvaas2.visible) {
       return; // Skip toggling if the same element is clicked and it's already visible
     }
 
-    if (targetObject) {
-      targetObject.visible = !targetObject.visible;
+    if (groupvaas2) {
+      groupvaas2.visible = !groupvaas2.visible;
+      cylinderObject.visible = true;
       group.visible = false;
-      targetObject3.visible = false;
+      groupvaas3.visible = false;
+
+      //background color change
+      const bkgrncolor = new THREE.Color(0xfaf4eb);
+      renderer.setClearColor(bkgrncolor);
+
+      //stand change
+      const newColor = new THREE.Color(0x88c5dc);
+      cylinderObject.material.color = newColor;
     }
 
     lastClickedElement = imageElement2; // Update the last clicked element
   }
 
   function toggleObjectVisibility3() {
-    if (lastClickedElement === imageElement3 && targetObject3.visible) {
+    if (lastClickedElement === imageElement3 && groupvaas3.visible) {
       return; // Skip toggling if the same element is clicked and it's already visible
     }
 
-    if (targetObject3) {
-      targetObject3.visible = !targetObject3.visible;
+    if (groupvaas3) {
+      groupvaas3.visible = !groupvaas3.visible;
       group.visible = false;
-      targetObject.visible = false;
+      groupvaas2.visible = false;
+      cylinderObject.visible = false;
+
+      //background color change
+      const bkgrncolor = new THREE.Color(0xebf4fa);
+      renderer.setClearColor(bkgrncolor);
+
+      //stand change
+      const newColor = new THREE.Color(0xb8abd4);
+      cylinderObject.material.color = newColor;
     }
 
-    lastClickedElement = imageElement3; // Update the last clicked element
+    lastClickedElement = imageElement3;
   }
 
   totalAnimationDuration = 5;
@@ -241,6 +328,30 @@ function updateAnimationAndColor() {
     childObject.material.color.copy(currentColor);
   }
 
+  if (initialColor1) {
+    const currentColor = initialColor1.clone();
+    // Calculate the new color based on the scroll position or time
+    // Example: linear interpolation between initial color and target color
+    if (scrollPosition >= 1700) {
+      const targetColor = new THREE.Color(0xb7acd2);
+      const t = Math.min((scrollPosition - 1700) / 1000, 1); // Ensure t is between 0 and 1
+      currentColor.lerp(targetColor, t);
+    }
+    childObject1.material.color.copy(currentColor);
+  }
+
+  if (initialColor2) {
+    const currentColor = initialColor2.clone();
+    // Calculate the new color based on the scroll position or time
+    // Example: linear interpolation between initial color and target color
+    if (scrollPosition >= 1700) {
+      const targetColor = new THREE.Color(0xf6f4cf);
+      const t = Math.min((scrollPosition - 1700) / 1000, 1); // Ensure t is between 0 and 1
+      currentColor.lerp(targetColor, t);
+    }
+    childObject2.material.color.copy(currentColor);
+  }
+  // Kleur van de bloemen veranderen
   const sphereColor = sphereInitialColor.clone();
   if (scrollPosition >= 1700) {
     const targetColor = new THREE.Color(0xb8abd4);
@@ -249,6 +360,7 @@ function updateAnimationAndColor() {
   }
   sphereObject.material.color.copy(sphereColor);
 
+  // Kleur van de cylindervormige stand veranderen
   const cylinderColor = cylinderInitialColor.clone();
   if (scrollPosition >= 2700) {
     const targetColor = new THREE.Color(0xb8abd4);
@@ -256,6 +368,28 @@ function updateAnimationAndColor() {
     cylinderColor.lerp(targetColor, t);
   }
   cylinderObject.material.color.copy(cylinderColor);
+
+  // Kleur van de cylindervormige stand veranderen
+  const cylinderColor1 = cylinderInitialColor1.clone();
+  if (scrollPosition >= 2700) {
+    const targetColor = new THREE.Color(0xb8abd4);
+    const t = Math.min((scrollPosition - 2700) / 1000, 1);
+    cylinderColor1.lerp(targetColor, t);
+  }
+  cylinderObject1.material.color.copy(cylinderColor1);
+
+  // de kleur blijft, alleen gaat niet terug naar wit.
+  /* function onScroll(scrollPosition) {
+  // Kleur van de cylindervormige stand veranderen
+  const cylinderColor = cylinderInitialColor.clone();
+  if (scrollPosition >= 2700) {
+    const targetColor = clickedCylinderColor; // Gebruik de kleur ingesteld door het klikken
+    const t = Math.min((scrollPosition - 2700) / 1000, 1);
+    cylinderColor.lerp(targetColor, t);
+  }
+  cylinderObject.material.color.copy(cylinderColor);
+}
+ */
 
   // Update the background color
   const backgroundColor = new THREE.Color(0xffffff);
@@ -266,13 +400,42 @@ function updateAnimationAndColor() {
   }
   renderer.setClearColor(backgroundColor);
 
-  //update sunlight position x
-  /*   if (scrollPosition >= 1000) {
-    sunLight.position.x = 1.5;
-  }
-  console.log(sunLight.position.x); */
-}
+  // verander sunlight position x
+  /*     const cylinderColor = cylinderInitialColor.clone();
+    if (scrollPosition >= 2700) {
+      const targetColor = new THREE.Color(0xb8abd4);
+      const t = Math.min((scrollPosition - 2700) / 1000, 1);
+      cylinderColor.lerp(targetColor, t);
+    }
+    cylinderObject.material.color.copy(cylinderColor); */
 
+  //update sunlight position x
+  /*   if (scrollPosition >= 3000) {
+    const targetsunlight = (sunLight.position.x = 2);
+    const t = Math.min((scrollPosition - 3000) / 1000, 1);
+    sunLight.position.lerp(targetsunlight, t);
+  }
+  console.log(sunLight.position.x);
+} */
+
+  // Definieer de begin- en eindpositie voor de lerping van sunlight
+  const startPosition = new THREE.Vector3(-2.5, 7, 3);
+  const targetPosition = new THREE.Vector3(2, 7, 3);
+
+  if (scrollPosition >= 4000) {
+    // Bereken de t-waarde voor de lerp tussen 0 en 1
+    const t = Math.min((scrollPosition - 4000) / 1000, 1);
+
+    // Bereken de nieuwe positie door te lerpen tussen startPosition en targetPosition met t als de factor
+    const newPosition = new THREE.Vector3();
+    newPosition.lerpVectors(startPosition, targetPosition, t);
+
+    // Update de positie van sunLight met de nieuwe waarde
+    sunLight.position.copy(newPosition);
+  }
+
+  console.log(sunLight.position.x);
+}
 // Call the updateAnimationAndColor function on scroll event
 window.addEventListener("scroll", updateAnimationAndColor);
 
