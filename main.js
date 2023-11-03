b0aadc02b6862b48a64468de6c70c382480f5c12
bobharterink <49344016+bobharterink@users.noreply.github.com>
2023-11-03 10:43:08 +0100

    juist

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ scene = new THREE.Scene();
 // Load GLB file using GLTFLoader
 const loader = new GLTFLoader();
 let sceneCamera = null;
-loader.load("/kijk5.glb", (gltf) => {
+loader.load("/juist.glb", (gltf) => {
   /*   objectGroup.add(gltf.scene);
    */ scene.add(gltf.scene);
   sceneCamera = gltf.scene.getObjectByName("test");
