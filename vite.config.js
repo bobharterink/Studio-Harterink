a424346df35a5c1c458911e3be432fe913941701
bobharterink <49344016+bobharterink@users.noreply.github.com>
2024-07-10 16:29:23 +0200

    tst

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import copy from "rollup-plugin-copy";
 
 export default defineConfig({
   build: {
@@ -8,6 +9,14 @@ export default defineConfig({
         main: resolve(__dirname, "index.html"),
         shipping: resolve(__dirname, "shipping.html"),
       },
+      plugins: [
+        copy({
+          targets: [
+            { src: "404.html", dest: "dist" },
+            { src: "_redirects", dest: "dist" },
+          ],
+        }),
+      ],
     },
   },
 });
